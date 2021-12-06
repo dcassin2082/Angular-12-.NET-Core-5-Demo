2ea2948a987f30c7b1fd03cf04d7addfb6230674
Daniel Cassin <dcassin2082@gmail.com>
2021-12-05 17:42:11 -0700

    Add files via upload

---
+++ shopping-cart/shared/shopping-cart.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/products/shared/product.Model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  constructor() { }
+
+  selectedProducts: Product[] = [];
+  selectedProduct: Product = new Product();
+  cartTotal: number = 0;
+  
+  filter: Product[];
+}
